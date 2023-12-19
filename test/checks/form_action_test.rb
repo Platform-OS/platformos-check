bc8352394a878f992efc380e3aea7cc1ddcb393c
Maciej Krajowski-Kukiel <krajek6@gmail.com>
2023-12-19 14:32:56 +0100

    add form action check

---
+++ test/checks/form_action_test.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module PlatformosCheck
+  class FormActionTest < Minitest::Test
+    def test_no_offense_with_proper_path
+      offenses = analyze_platformos_app(
+        FormAction.new,
+        "app/views/pages/index.liquid" => <<~END
+          <form action="/dummy/create">
+          </form>
+        END
+      )
+
+      assert_offenses("", offenses)
+    end
+
+    def test_reports_invalid_action
+      offenses = analyze_platformos_app(
+        FormAction.new,
+        "app/views/pages/index.liquid" => <<~END
+          <form action="dummy/create">
+          </form>
+        END
+      )
+
+      assert_offenses(<<~END, offenses)
+        Use action="/dummy/create" (start with /) to ensure the form can be submitted multiple times in case of validation errors at app/views/pages/index.liquid:1
+      END
+    end
+
+    def test_no_offense_when_action_dynamic
+      offenses = analyze_platformos_app(
+        FormAction.new,
+        "app/views/pages/index.liquid" => <<~END
+          {% assign var = "/dummy/create" %}
+          <form action="{{ var }}">
+          </form>
+        END
+      )
+
+      assert_offenses("", offenses)
+    end
+  end
+end
