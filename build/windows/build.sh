b32b248a02f721ac96cd40e0fd490011532ae53f
piotrze <piotr.bliszczyk@gmail.com>
2024-04-10 13:03:38 +0200

    Fix

--- build/windows/build.sh
+++ build/windows/build.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+# --gem-file /c/Ruby32-x64/bin/etc/ssl \
 
 ocran run.rb --output lsp.exe --add-all-core --gemfile ./Gemfile --no-dep-run --gem-full --gem-full=platformos-check --console \
---gem-file /c/Ruby32-x64/bin/etc/ssl \
 --dll ruby_builtin_dlls/libyaml-0-2.dll \
 --dll ruby_builtin_dlls/zlib1.dll \
 --dll ruby_builtin_dlls/libssl-3-x64.dll \
